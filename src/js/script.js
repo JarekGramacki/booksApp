79ee7d0b15e70e846c32063f19c0102f3d7eb3cf
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-05-10 22:46:44 +0200

    Add function render and initActions

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,50 @@
 
+'use strict';
 
+{
+  const select = {
+    templateOf: {
+      templateBooks: "#template-book"
+    },
+    panel: {
+      booksPanel: ".books-list",
+      bookImage: ".book__image"
+    },    
 
-    
-    for(let elementBooks of dataSource.books){
-        console.log(elementBooks)
+  };
+  
+  const templates = {
+    // kompilujemy template z indexu po id (funkcja Handle.compile zwraca funkcję która później 
+    // może przyjąć obiekt jako parametr i podstawić dane do templateu) - dane podstawa w placeholdery  {{}}
+    templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBooks).innerHTML)
+  }
+
+   function render(){ 
+    for(let book of dataSource.books){
+
+      const bookGeneratedHTML = templates.templateBook(book); // generujemy html z podstawionymi danymi ksiazki zamaist placeholderow
+      const bookDOMElement = utils.createDOMFromHTML(bookGeneratedHTML); // tworzymy obiekt DOM zamiast czystego tekstu html
+
+      const booksPanel = document.querySelector(select.panel.booksPanel); // szukamy miejsca gdzie dorzucić książki
+      booksPanel.appendChild(bookDOMElement); // dodajemy elemnt dom do panelu
+
     }
+  }
+  render();
+  initActions();
+
+  const favoriteBooks = [];
+
+  function initActions(){
+    const booksImages = document.querySelectorAll(select.panel.booksPanel + " " + select.panel.bookImage);
+
+    for(let bookImageElement of booksImages){
+      bookImageElement.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        const bookImg = this; 
+        bookImg.classList.add('favorite');
+      })
+    }
+  }
+}
 
