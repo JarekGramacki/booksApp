856cda180865705671c57ad15bec4be33c8d36ac
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-05-11 21:48:12 +0200

    Add function render

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,37 @@
 
-'use strict';
-
 {
+  'use strict'
+
   const select = {
+
     templateOf: {
-      templateBooks: "#template-book"
+      templateBook: "#template-book"
     },
+
     panel: {
-      booksPanel: ".books-list",
-      bookImage: ".book__image"
-    },    
-
+      productsList: ".books-list"
+    }
   };
+
   
-  const templates = {
-    // kompilujemy template z indexu po id (funkcja Handle.compile zwraca funkcję która później 
-    // może przyjąć obiekt jako parametr i podstawić dane do templateu) - dane podstawa w placeholdery  {{}}
-    templateBook: Handlebars.compile(document.querySelector(select.templateOf.templateBooks).innerHTML)
-  }
+   const menuProductTemplate = Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML); 
+  
 
-   function render(){ 
-    for(let book of dataSource.books){
 
-      const bookGeneratedHTML = templates.templateBook(book); // generujemy html z podstawionymi danymi ksiazki zamaist placeholderow
-      const bookDOMElement = utils.createDOMFromHTML(bookGeneratedHTML); // tworzymy obiekt DOM zamiast czystego tekstu html
-
-      const booksPanel = document.querySelector(select.panel.booksPanel); // szukamy miejsca gdzie dorzucić książki
-      booksPanel.appendChild(bookDOMElement); // dodajemy elemnt dom do panelu
+  function render(){
+    for (let book of dataSource.books){
+    
+    const generatedHTML = menuProductTemplate(book);
+    console.log("kod html:",generatedHTML)
+    const elementDOM = utils.createDOMFromHTML(generatedHTML);  
+    console.log("obiekt DOM:",elementDOM)
+    const menuContainer = document.querySelector(select.panel.productsList); 
+    console.log("kontener z lista ksiazek:",menuContainer)
+    menuContainer.appendChild(elementDOM);
 
     }
   }
-  render();
-  initActions();
+  render()
 
-  const favoriteBooks = [];
-
-  function initActions(){
-    const booksImages = document.querySelectorAll(select.panel.booksPanel + " " + select.panel.bookImage);
-
-    for(let bookImageElement of booksImages){
-      bookImageElement.addEventListener('dblclick', function(event){
-        event.preventDefault();
-        const bookImg = this; 
-        bookImg.classList.add('favorite');
-      })
-    }
-  }
 }
 
