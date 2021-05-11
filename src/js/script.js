8d581bccd5f5d8b07994c938a959d52fb311d415
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-05-11 22:49:09 +0200

    Add function render with coments

--- src/js/script.js
+++ src/js/script.js
@@ -21,13 +21,13 @@
   function render(){
     for (let book of dataSource.books){
     
-    const generatedHTML = menuProductTemplate(book);
+    const generatedHTML = menuProductTemplate(book); //tworzymy czysty kod html ktory jest polaczeniem szablonu template oraz danych z data.js
     console.log("kod html:",generatedHTML)
-    const elementDOM = utils.createDOMFromHTML(generatedHTML);  
+    const elementDOM = utils.createDOMFromHTML(generatedHTML);  //na podstawie tego stworzenoego kodu html tworzymy jeden obiekt DOM (jedna ksiazke)
     console.log("obiekt DOM:",elementDOM)
-    const menuContainer = document.querySelector(select.panel.productsList); 
+    const menuContainer = document.querySelector(select.panel.productsList);  //szukamy miejsca przy pomocy selektora gdzie dorzucic liste ksiazek 
     console.log("kontener z lista ksiazek:",menuContainer)
-    menuContainer.appendChild(elementDOM);
+    menuContainer.appendChild(elementDOM); //dodajemy ksiazki za pomoca petli ppojedynczo  jako elemnty DOM do listy 
 
     }
   }
