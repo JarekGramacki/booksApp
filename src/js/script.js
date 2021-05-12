b414adc5320ed7626cbb1ddb1376fe6d455b3ebf
Jarek Gramacki <gramacki.programowanie@gmail.com>
2021-05-12 21:56:46 +0200

    Add add not done function initAction

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,49 @@
 
 {
-  'use strict'
+  'use strict';
 
   const select = {
 
     templateOf: {
-      templateBook: "#template-book"
+      templateBook: '#template-book'
     },
 
     panel: {
-      productsList: ".books-list"
+      productsList: '.books-list',
+      productsImage: '.book__image'
     }
   };
 
   
-   const menuProductTemplate = Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML); 
+  const menuProductTemplate = Handlebars.compile(document.querySelector(select.templateOf.templateBook).innerHTML); 
   
 
 
   function render(){
     for (let book of dataSource.books){
     
-    const generatedHTML = menuProductTemplate(book); //tworzymy czysty kod html ktory jest polaczeniem szablonu template oraz danych z data.js
-    console.log("kod html:",generatedHTML)
-    const elementDOM = utils.createDOMFromHTML(generatedHTML);  //na podstawie tego stworzenoego kodu html tworzymy jeden obiekt DOM (jedna ksiazke)
-    console.log("obiekt DOM:",elementDOM)
-    const menuContainer = document.querySelector(select.panel.productsList);  //szukamy miejsca przy pomocy selektora gdzie dorzucic liste ksiazek 
-    console.log("kontener z lista ksiazek:",menuContainer)
-    menuContainer.appendChild(elementDOM); //dodajemy ksiazki za pomoca petli ppojedynczo  jako elemnty DOM do listy 
+      const generatedHTML = menuProductTemplate(book); //tworzymy czysty kod html ktory jest polaczeniem szablonu template oraz danych z data.js
+      //console.log('kod html:',generatedHTML);
+      const elementDOM = utils.createDOMFromHTML(generatedHTML);  //na podstawie tego stworzenoego kodu html tworzymy jeden obiekt DOM (jedna ksiazke)
+      //console.log('obiekt DOM:',elementDOM);
+      const menuContainer = document.querySelector(select.panel.productsList);  //szukamy miejsca przy pomocy selektora gdzie dorzucic liste ksiazek 
+      //console.log('kontener z lista ksiazek:',menuContainer);
+      menuContainer.appendChild(elementDOM); //dodajemy ksiazki za pomoca petli ppojedynczo  jako elemnty DOM do listy 
 
-    }
-  }
-  render()
+    };
+  };
+  render();
+  initAction();
 
-}
+  favoriteBooks = [];
+
+  function initAction (){
+
+    
+
+   
+
+  };
+
+};
 
